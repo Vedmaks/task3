afcf0ae5eb418b08db6b338d1542d253e562c245
Vedmaks <marchenkovmaxim0502@gmail.com>
2020-09-27 15:10:19 +0400

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -12,57 +13,78 @@ import (
 
 var dir string
 var usingFile string
+var links []string
 
 func main() {
 
-	fmt.Print("Set file to use: ")
+	//Создание лога
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	log.SetOutput(file)
+
+	//Прием данных пользователя
+	fmt.Print("Выберите файл с ссылками: ")
 	var setFile string
 	fmt.Scan(&setFile)
 	usingFile = setFile
 
-	fmt.Print("Select dir: ")
+	fmt.Print("Укажите директорию для сохранения: ")
 	var selectDir string
 	fmt.Scan(&selectDir)
-	os.Mkdir(selectDir, 0777)
+	os.MkdirAll(selectDir, 0777)
 	dir = selectDir
 
-	createFile(20)
+	log.Println("Директория для сохранения: " + selectDir)
+	log.Println("Файл с ссылками: " + setFile)
 
+	createFile()	// Создание файла
 }
 
-func createFile(num int) {
+func createFile() {
 
-	for i := 0; i < num; i++ {
+	setLink()	// Чтение ссылок из файла
 
-		file, err := os.Create(setName())
+	for i := 0; i < len(links); i++ {
+
+		file, err := os.Create(setName())	// Создание файла с уникальным именем
+
 		if err != nil {
-			fmt.Println("Unable to create file:", err)
-			os.Exit(2)
+			log.Fatal("Невозможно создать файл: ", err)
 		}
+
 		defer file.Close()
 
-		file.WriteString(getData(i))
-		fmt.Println("Файл создан: " + file.Name())
-		fmt.Println("Всего создано:")
-		fmt.Println(i + 1)
+		file.WriteString(getData(i))	// Запись данных в файл
 	}
 
+	fmt.Println("Создание файлов завершено успешно.")
+	log.Println("Создание файлов завершено успешно.")
 }
 
-func getData(num int) string {
+func getData(i int) string {
 
-	resp, err := http.Get(setLink(num))
+	resp, err := http.Get(links[i])
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Ошибка доступа к ссылке, подробности в логе.")
+		log.Print("Ссылка не доступна!", err)
 
+		return ""
 	}
+
 	defer resp.Body.Close()
 
 	var data string
 
-	for true {
+	for {
 
-		bs := make([]byte, 1014)
+		bs := make([]byte, 1024)
 		n, err := resp.Body.Read(bs)
 
 		if n == 0 || err != nil {
@@ -75,49 +97,50 @@ func getData(num int) string {
 	return data
 }
 
-func setLink(num int) string {
+func setLink() {
 
 	file, err := os.Open(usingFile)
+
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fmt.Println("Ошибка открытия файла, подробности в логе.")
+		log.Fatal("Ошибка открытия файла: ", err)
 	}
+
 	defer file.Close()
 
-	var links string
+	log.Println("Файл найден. Считываем ссылки...")
+
+	var linkFile string
 	data := make([]byte, 64)
 
 	for {
 		n, err := file.Read(data)
 		if err == io.EOF {
+			log.Println("Считывание ссылок завершено.")
 			break
 		}
-		links += (string(data[:n]))
+		linkFile += (string(data[:n]))
 	}
 
-	linkNum := strings.Fields(links)
-
-	if len(linkNum) == num+1 {
-
-		os.Exit(1)
-	}
-
-	return linkNum[num]
-
+	links = strings.Fields(linkFile)
 }
 
 func setName() string {
+
 	var fullName string
 
 	rand.Seed(time.Now().UnixNano())
 	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
 		"abcdefghijklmnopqrstuvwxyzåäö" +
 		"0123456789")
+		
 	length := 8
 	var b strings.Builder
+
 	for i := 0; i < length; i++ {
 		b.WriteRune(chars[rand.Intn(len(chars))])
 	}
+
 	str := b.String()
 
 	fullName = dir + "/" + str + ".html"
